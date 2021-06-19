7529beb0067ef3e73e1d00fa53290856b5583ff2
terry <terry@hexacube.co.nz>
2021-06-19 16:07:46 +0000

    added show script

---
+++ bee/cashout-show.sh
@@ -0,0 +1,5 @@
+# Add your nodes debug api here
+./cashout.sh list-uncashed http://localhost:11635
+./cashout.sh list-uncashed http://localhost:12635
+./cashout.sh list-uncashed http://localhost:13635
+
