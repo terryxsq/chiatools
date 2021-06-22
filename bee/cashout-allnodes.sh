0fc060ae96f048d43dd1988e8b56b75893f5caff
terry <terry@hexacube.co.nz>
2021-06-22 04:05:46 +0000

    updated cashout scripts

--- bee/cashout-allnodes.sh
+++ bee/cashout-allnodes.sh
@@ -1,3 +1,17 @@
 # Add your nodes debug api here
-./cashout.sh cashout-all http://localhost:1635
-#./cashout.sh cashout-all http://localhost:1735
+./cashout.sh cashout-all http://localhost:11635
+./cashout.sh cashout-all http://localhost:12635
+./cashout.sh cashout-all http://localhost:13635
+./cashout.sh cashout-all http://localhost:14635
+./cashout.sh cashout-all http://localhost:15635
+./cashout.sh cashout-all http://localhost:16635
+./cashout.sh cashout-all http://localhost:17635
+./cashout.sh cashout-all http://localhost:18635
+./cashout.sh cashout-all http://localhost:19635
+./cashout.sh cashout-all http://localhost:20635
+./cashout.sh cashout-all http://localhost:21635
+./cashout.sh cashout-all http://localhost:22635
+./cashout.sh cashout-all http://localhost:23635
+./cashout.sh cashout-all http://localhost:24635
+./cashout.sh cashout-all http://localhost:25635
+
--- bee/cashout-show.sh
+++ bee/cashout-show.sh
@@ -2,4 +2,16 @@
 ./cashout.sh list-uncashed http://localhost:11635
 ./cashout.sh list-uncashed http://localhost:12635
 ./cashout.sh list-uncashed http://localhost:13635
+./cashout.sh list-uncashed http://localhost:14635
+./cashout.sh list-uncashed http://localhost:15635
+./cashout.sh list-uncashed http://localhost:16635
+./cashout.sh list-uncashed http://localhost:17635
+./cashout.sh list-uncashed http://localhost:18635
+./cashout.sh list-uncashed http://localhost:19635
+./cashout.sh list-uncashed http://localhost:20635
+./cashout.sh list-uncashed http://localhost:21635
+./cashout.sh list-uncashed http://localhost:22635
+./cashout.sh list-uncashed http://localhost:23635
+./cashout.sh list-uncashed http://localhost:24635
+./cashout.sh list-uncashed http://localhost:25635
 
