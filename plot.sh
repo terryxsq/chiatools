2282258c9dc429e74eb293eedecf1cbbc96e2824
Your Name <you@example.com>
2021-06-22 13:56:34 +0000

    added madmax plot script

---
+++ plot.sh
@@ -0,0 +1,3 @@
+# sh plot.sh [count] [bucket] [destination]
+~/chia-plotter/build/chia_plot -k 33 -n $1 -r 13 -u $2 -t /mnt/ssd/tmp/ -2 /mnt/ssd/tmp/ -d $3 -p adbce405d702c00612df68f0e40c36121b1abcf0f73effc6a48ca6fed087b50932d286f90f1ea2e26107a19f7cd657b5 -f aefb32cb93cf8b72ab6183929c8a6ca0c15c40c4262052c55ffbdac2c991cc80b03b474713bda493e2c60daf9b3ee6d9
+
